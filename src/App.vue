b7ff73f65ac955998b400353dd4a88b9ea72204d
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-09-12 12:12:02 +0700

    Generic nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
-  <main>
-    <router-view />
-  </main>
+  <div id="app">
+    <NavBar />
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style>
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: "Roboto", sans-serif;
 }
 
 #app {
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   background-color: #f5f5f5;
+  min-height: 100vh;
 }
 
 main {
   max-width: 1200px;
   width: 90%;
+  margin: 0 auto;
   border-radius: 5px;
-  height: 100vh;
   background-color: white;
   border: 1px solid #ccc;
   border-radius: 5px;
   display: flex;
   justify-content: center;
+  padding-top: 60px;
+  min-height: 100vh;
 }
 
 h1 {
