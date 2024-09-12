30b116f81a9d8fd34b33ec89c03a8a6ac5ba8770
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-09-12 10:46:26 +0700

    Router and folder layout

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import GameSelection from "../views/GameSelection.vue";
+import WordScrambleCategories from "../views/WordScramble/WordScrambleCategories.vue";
+import WordScrambleGame from "../views/WordScramble/WordScrambleGame.vue";
+
+const routes = [
+  { path: "/", name: "GameSelection", component: GameSelection },
+  {
+    path: "/word-scramble/categories",
+    name: "WordScrambleCategories",
+    component: WordScrambleCategories,
+  },
+  {
+    path: "/word-scramble/game",
+    name: "WordScrambleGame",
+    component: WordScrambleGame,
+  },
+  // More routes for other games can be added here
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
