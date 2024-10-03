5bb4aff4d6c863bb50accd1915e011156a53f7ad
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-10-03 08:53:43 +0700

    Importing code word state

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 import wordScramble from "./modules/wordScramble";
 import flagFinder from "./modules/flagFinder";
 import quizMaster from "./modules/quizMaster";
+import codeWord from "./modules/codeWord";
 
 export default createStore({
   modules: {
     wordScramble,
     quizMaster,
     flagFinder,
+    codeWord,
   },
 });
