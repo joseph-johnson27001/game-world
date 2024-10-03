08985d39e934176f92e57bae5fd46de75ce617dd
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-10-03 12:05:18 +0700

    Results layout

--- src/views/CodeWord/CodeWordResults.vue
+++ src/views/CodeWord/CodeWordResults.vue
@@ -1,2 +1,33 @@
-<template>RESULTS</template>
->
+<template>
+  <div class="code-word-results">
+    <div class="heading-container">
+      <h1>{{ resultMessage }}</h1>
+    </div>
+
+    <p v-if="this.success">You had {{ attempts }} attempts remaining.</p>
+    <p v-else>You ran out of attempts and were captured.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    attempts() {
+      return this.$route.query.attempts;
+    },
+    success() {
+      return this.$route.query.success === "true";
+    },
+    resultMessage() {
+      return this.success ? "You have escaped!" : "You failed!";
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+.code-word-results {
+  text-align: center;
+}
+</style>
